198b0f7f7ecf6ff42ba700afb2e5c0ebb2b4169d
Gotprgmer <gofiction95@gmail.com>
2024-07-30 23:40:42 +0900

    add simple http

---
+++ src/com/mj/ClientMain.java
@@ -0,0 +1,37 @@
+package com.mj;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientMain {
+    public ClientMain() {
+    }
+
+    public static void main(String[] args) {
+        try {
+            Client client = new Client();
+
+            while(true) {
+                Socket clientSocket = null;
+
+                try {
+                    clientSocket = client.connect("localhost", 1992);
+                    client.request(clientSocket);
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    if (clientSocket != null && !clientSocket.isClosed()) {
+                        try {
+                            clientSocket.close();
+                        } catch (Exception e) {
+                            System.out.println("Closing http Error was caused" + e.getMessage());
+                        }
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/com/mj/Server.java
@@ -0,0 +1,34 @@
+package com.mj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public Server() {
+    }
+
+    public ServerSocket connectInit(int port) throws IOException {
+        return this.serverSocket = new ServerSocket(port);
+    }
+
+    public void response(Socket socket) throws IOException {
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String msg = in.readLine();
+        System.out.println(msg);
+        System.out.println("Received from Client");
+
+        String response = "Hello, client";
+        out.println(response);
+        System.out.println("Response sent to Client");
+    }
+}
